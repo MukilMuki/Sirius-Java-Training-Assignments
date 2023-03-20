55628c60b7d6f5fa573d9d6b9522040f9af95862
MukilMuki <mukilan.shanmugasundaram@cdw.com>
2023-03-20 11:00:37 +0530

    Assignment-9 Changed

--- Assignment-2/Exercise9.java
+++ Assignment-2/Exercise9.java
@@ -1,35 +1,53 @@
 package day_2;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Exercise9 {
 	public static void main(String args[]){
-		int a,b = 0,c,i,j=0;
-		while(j==0) {
+		int b = 0,c,i = 0,j=0;
+		int a[]=new int[10];
+		int sum=0;
+		System.out.println("Enter the marks of 10 students");
+		while(i<10) {
 			try {
 				Scanner scan= new Scanner(System.in);
-				System.out.println("Enter the marks of 10 students");
-				for(i=0;i<10;i++) {
-					a=scan.nextInt();
-					if(a<0)
-						throw new CustomExceptionCheck("Negative Values");
-					if(a>100)
-						throw new CustomExceptionCheck("The Mark range should be inbetween 0-100");
-					b+=a;
-				}
-					c=b/10;
-					System.out.println(c);
+					a[i]=scan.nextInt();
+					if(a[i]<0) {
+						throw new NegativeValueException("Negative Values");
+					}
+					if(a[i]>100) {
+						throw new OutofRangeException("The Mark range should be inbetween 0-100");
+					}
+					System.out.println(i);
+				i++;
 			}
-			catch(Exception e) {
-				System.out.println(e);
+			catch(NegativeValueException e) {
+				System.out.println(e.getMessage());
+			}
+			catch(OutofRangeException e) {
+				System.out.println(e.getMessage());
+			}
+			catch(InputMismatchException e){
+				System.out.println("Enter Correct value");
 			}
 		}
+		for(int value:a)
+			sum+=value;
+		System.out.println(sum);
 	}
 }
 
-class CustomExceptionCheck extends Exception{
+class NegativeValueException extends Exception{
 	
-	CustomExceptionCheck(String message){
+	NegativeValueException(String message){
+		super(message);
+	}
+}
+
+class OutofRangeException extends Exception{
+	
+	OutofRangeException(String message){
 		super(message);
 	}
 }
